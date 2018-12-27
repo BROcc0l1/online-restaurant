06da2857d9b6415041896807bb262704374e5267
BROcc0l1 <xxxmakxxx777@gmail.com>
2018-12-27 06:50:38 +0200

    Add auto product removing

--- php/chef_take_order.php
+++ php/chef_take_order.php
@@ -6,7 +6,7 @@ session_start();
 $con = mysqli_connect('localhost', 'root', '', 'online_restaurant');
 if (!$con) die('Could not connect: ' . mysqli_error($con));
 
-if (isset($_GET['take_order_button'])) {
+#if (isset($_POST['take_order_button'])) {
 
 $order_id = $_SESSION['order_id'];
 
@@ -26,7 +26,6 @@ $foods_data = mysqli_fetch_assoc($foods_query_result);
 $food_ids = explode(" ", $foods_data['foods_ids']);
 
 // get food names by IDs
-
 $food_names = array();
 
 foreach ($food_ids as $id) {
@@ -42,6 +41,31 @@ foreach ($food_ids as $id) {
 }
 
 
+// remove products from warehouse
+foreach ($food_ids as $fid) {
+	$get_food_products_query = "SELECT product_id, product_quantity FROM `menu_ingredients` WHERE menu_item_id='$fid'";
+	$get_food_products_query_result = mysqli_query($con, $get_food_products_query);
+	
+	while ($row = mysqli_fetch_row($get_food_products_query_result)) {
+
+		$id = $row[0];
+		$quan = $row[1];
+
+		$get_quantity_query = "SELECT quantity FROM `warehouse` WHERE id='$id' LIMIT 1";
+		$quantity_query_result = mysqli_fetch_assoc(mysqli_query($con, $get_quantity_query));
+		$quantity = $quantity_query_result['quantity'];
+		echo $quantity; echo " "; echo $quan; echo " ";
+		$quantity -= $quan;
+		echo $quantity; echo "<br><br>";
+
+		$update_quantity_query = "UPDATE `warehouse` SET quantity='$quantity' WHERE id='$id'";
+		mysqli_query($con, $update_quantity_query);
+
+	}
+
+}
+
+
 echo $order_id;
 echo '<br><br>';
 foreach ($food_names as $fn) {
@@ -49,7 +73,7 @@ foreach ($food_names as $fn) {
 	echo '<br>';
 }
 
-}
+#}
 
 mysqli_close($con);
 
@@ -63,7 +87,7 @@ mysqli_close($con);
 <body>
 
 	<form action="chef_end_order.php">
-		<button name="end_order_button"> Finish order </button>
+		<button> Finish order </button>
 	</form>
 
 </body>
